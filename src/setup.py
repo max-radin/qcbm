27a6e1ef7839728e14d408e89282ab9ea1416f02
Max Radin <radin.max@gmail.com>
2020-05-05 13:48:48 -0400

    Create setup.py

---
+++ src/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+import os
+
+readme_path = os.path.join("..", "README.md")
+with open(readme_path, "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="quantum-qcbm",
+    version="0.1.0",
+    author="Zapata Computing, Inc.",
+    author_email="info@zapatacomputing.com",
+    description="QCBM package for Orquestra.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_namespace_packages(include=['zquantum.*']),
+    package_dir={'' : 'python'},
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ),
+    install_requires=[
+        'z-quantum-core',
+    ]
+)
